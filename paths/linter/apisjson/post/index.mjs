2ccb393b61fd3642971bd1940ba4b0e6d22197ab
Kin Lane <kinlane@gmail.com>
2023-08-09 18:38:50 -0400

    debug

--- paths/linter/apisjson/post/index.mjs
+++ paths/linter/apisjson/post/index.mjs
@@ -5,28 +5,7 @@ import { truthy } from "@stoplight/spectral-functions";
 
 export function handler(event, context, callback) {
 
-  const myDocument = new Document(event.body);
+  console.log(event.body);
 
-  const spectral = new Spectral();
-  spectral.setRuleset({
-
-    rules: {
-
-      "apisjson-description": {
-        given: "$.description",
-        message: "Description must not be empty",
-        then: {
-          function: truthy,
-        },
-      },
-
-    }, 
-
-  });
-
-  spectral.run(myDocument).then(results => {
-    console.log("here are the results", results);
-    callback(null,results);
-  });
 
 };
