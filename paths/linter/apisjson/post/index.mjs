7a127cf54f89c43ccfaef0e5c7c4f4108eaa3ec1
Kin Lane <kinlane@gmail.com>
2023-08-09 19:15:05 -0400

    debug

--- paths/linter/apisjson/post/index.mjs
+++ paths/linter/apisjson/post/index.mjs
@@ -3,8 +3,16 @@ const { Spectral, Document } = spectralCore;
 import Parsers from "@stoplight/spectral-parsers";
 import { truthy } from "@stoplight/spectral-functions"; 
 import yaml from 'js-yaml';
+import mysql from '';
 
 export function handler(event, context, callback) {
+
+  var connection = mysql.createConnection({
+    host     : process.env.host,
+    user     : process.env.user,
+    password : process.env.password,
+    database : process.env.database
+    });  
 
   const apisjson = event.body;
 
