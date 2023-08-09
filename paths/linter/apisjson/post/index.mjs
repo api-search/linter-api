c6f1a61dc3050f2f95a8a297ceede9bca6b82739
Kin Lane <kinlane@gmail.com>
2023-08-09 18:42:28 -0400

    debug

--- paths/linter/apisjson/post/index.mjs
+++ paths/linter/apisjson/post/index.mjs
@@ -7,5 +7,28 @@ export function handler(event, context, callback) {
 
   console.log(event.body);
 
+  const myDocument = new Document(event.body);
+
+  const spectral = new Spectral();
+  spectral.setRuleset({
+
+    rules: {
+
+      "apisjson-description": {
+        given: "$.description",
+        message: "Description must not be empty",
+        then: {
+          function: truthy,
+        },
+      },
+
+    }, 
+
+  });
+
+  spectral.run(myDocument).then(results => {
+    console.log("here are the results", results);
+    callback(null,results);
+  });
 
 };
