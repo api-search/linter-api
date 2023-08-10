785d8ceddd3e2da5dd21efb9f0fc90ae58ae47d1
Kin Lane <kinlane@gmail.com>
2023-08-10 19:13:24 -0400

    roilling back to static

--- paths/linter/apisjson/post/index.mjs
+++ paths/linter/apisjson/post/index.mjs
@@ -16,39 +16,108 @@ export function handler(event, context, callback) {
 
   const apisjson = event.body;
 
-  var sql = "SELECT rule FROM rules";
-  connection.query(sql, function (error, results, fields) { 
+    var sql = "SELECT rule FROM rules";
+    connection.query(sql, function (error, results, fields) { 
 
-    var r = results;
-    var ro = {};
+      var rules = {
+        
+        rules: {
+          
+          "apis-json-v14-name": {
+            description: "Name of APIs.json",
+            given: "$",
+            message: "Providing a name of your index of APIs helps ensure it will be discoverable.",
+            severity: "error",
+            then: {
+              field: "name",
+              function: truthy,
+            },
+          },
+          "apis-json-v14-description": {
+            description: "Description of APIs.json",
+            given: "$",
+            message: "A robust and informative description of your colleciton of APIs makes a lot sense.",
+            severity: "error",
+            then: {
+              field: "description",
+              function: truthy,
+            },
+          },    
+          "apis-json-v14-description-empty": {
+            description: "Empty Description of APIs.json",
+            given: "$.description",
+            message: "A robust and informative description of your colleciton of APIs makes a lot sense.",
+            severity: "error",
+            then: {
+              function: truthy,
+            },
+          },                  
+          "apis-json-v14-image": {
+            description: "Image of APIs.json",
+            given: "$",
+            message: "It makes your API more presentable to have a logo or image representation.",
+            severity: "error",
+            then: {
+              field: "image",
+              function: truthy,
+            },
+          },    
+          "apis-json-v14-image-empty": {
+            description: "Empty Image of APIs.json",
+            given: "$.image",
+            message: "It makes your API more presentable to have a logo or image representation.",
+            severity: "error",
+            then: {
+              function: truthy,
+            },
+          },   
+          "apis-json-v14-image-url": {
+            description: "Empty Image of APIs.json",
+            given: "$.image",
+            message: "It makes your API more presentable to have a logo or image representation.",
+            severity: "error",
+            then: {
+              function: "pattern",
+              functionOptions: {
+                notMatch: "^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$"
+              },
+            },
+          },                    
+          "apis-json-v14-tags": {
+            description: "Tags for APIs.json",
+            given: "$",
+            message: "Using the tags property for your APIs.json helps add more metadata and make your APIs discoverable.",
+            severity: "error",
+            then: {
+              field: "tags",
+              function: truthy,
+            },
+          },
+          "apis-json-v14-tags-one": {
+            description: "One Tag for APIs.json",
+            given: "$",
+            message: "Having at least one tag for your APIs.json helps ensure that it will be more discoverable.",
+            severity: "error",
+            then: {
+              function: "pattern",
+              functionOptions: {
+                min: 1
+              },
+            },
+          }, 
+                              
+        },
+        
+      };
 
-    var rules = '{\r\n';
-    rules += 'rules: {\r\n';
-
-    r.forEach(function(row) {
-
-      ro = yaml.load(row.rule); 
-
-      var rule_name = Object.keys(ro);
-      
-      rules += '"' + rule_name + '": {\r\n';
-      rules += 'given: "' + ro[rule_name].given + '",\r\n';
-      rules += 'message: "' + ro[rule_name].description + '",\r\n';
-      rules += 'then: {\r\n';
-      rules += 'function: truthy,\r\n';
-      rules += '},\r\n';
-      rules += '},\r\n';    
-
-    });
-  
-    rules += '},\r\n';
-    rules += '}\r\n';  
+      const spectral = new Spectral();
+      spectral.setRuleset(rules);      
     
-    const spectral = new Spectral();
-    spectral.setRuleset(eval(rules));    
-
-    connection.end();
-    callback(null,rules);    
+      spectral.run(apisjson).then(results => {
+        console.log("here are the results", results);
+        callback(null,results);
+      });
+  
 
   });
 
