1933d1649c7cf3ec4b27fe05f8dc869361fdc199
Kin Lane <kinlane@gmail.com>
2023-08-10 21:07:27 -0400

    debug

--- paths/linter/apisjson/post/index.mjs
+++ paths/linter/apisjson/post/index.mjs
@@ -164,20 +164,6 @@ export function handler(event, context, callback) {
               field: "tags",
               function: truthy,
             },
-          },   
-          
-          "apis-json-v14-apis-tags": {
-            description: "Tags for API",
-            message: "Using the tags property for your APIs helps add more metadata and make your APIs discoverable.",
-            given: "$.apis.*",
-            severity: "error",
-            then: {
-              field: "url",
-              function: "pattern",
-              functionOptions: { 
-                match: "^((http|https)://)[-a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)$",
-              },
-            },
           },                    
                 
           "apis-json-v14-apis-properties-documentation": {
